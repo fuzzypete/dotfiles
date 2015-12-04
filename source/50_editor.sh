9bf3a2dcd8bc42e01a840d259f2a116c744f28e4
Peter Wickersham <pwickersham@i340b.com>
2015-12-04 11:27:52 -0800

    updated for my preferences

--- source/50_editor.sh
+++ source/50_editor.sh
@@ -1,13 +1,13 @@
 # Editing
 
-if [[ ! "$SSH_TTY" ]] && is_osx; then
-  export EDITOR='mvim'
-  export LESSEDIT='mvim ?lm+%lm -- %f'
-else
-  export EDITOR='vim'
-fi
+#if [[ ! "$SSH_TTY" ]] && is_osx; then
+#  export EDITOR='mvim'
+#  export LESSEDIT='mvim ?lm+%lm -- %f'
+#else
+#  export EDITOR='vim'
+#fi
 
-export VISUAL="$EDITOR"
-alias q="$EDITOR"
-alias qv="q $DOTFILES/link/.{,g}vimrc +'cd $DOTFILES'"
-alias qs="q $DOTFILES"
+#export VISUAL="$EDITOR"
+#alias q="$EDITOR"
+#alias qv="q $DOTFILES/link/.{,g}vimrc +'cd $DOTFILES'"
+#alias qs="q $DOTFILES"
